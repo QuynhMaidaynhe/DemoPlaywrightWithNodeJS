800ea34c8a37166a8162360bb6df6067a1a89349
Quynhmaidaynhe <lequynhmai271099@gmail.com>
2022-10-11 11:21:52 +0700

    first commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,17 +1,28 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('demo playwright', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
 
-  // create a locator
-  const text = await page.locator('//input[@id="autocomplete"]');
+  // radiobutton
+  const radio1 = await page.locator('input[value="radio1"]'); 
+  const radio2 = await page.locator('input[value="radio2"]'); 
+  const radio3 = await page.locator('input[value="radio3"]'); 
+  expect(await radio1.isChecked()).toBeFalsy();
+  expect(await radio2.isChecked()).toBeFalsy();
+  expect(await radio3.isChecked()).toBeFalsy();
+  await radio2.click();
+  expect(await radio2.isChecked()).toBeTruthy();
 
-  // Click the get started link.
-  await text.type('Aus',);
-  
-  let data = await page.locator('//div[@class="ui-menu-item-wrapper"]').allTextContents()
-  console.log(data)
-  // Expects the URL to contain intro.
+  // search
+  const text = await page.locator('//input[@id="autocomplete"]');
+  await text.type('Aus');
+  const suggesttext = await page.locator('//li[@class="ui-menu-item"]');
+  await expect(suggesttext).toContainText(['Australia', 'Austria']);
   // await expect(page.locator('div[class="ui-menu-item-wrappe"]')).toHaveValue('Austria');
+
+  // dropdownlist
+  const dropdown = await page.locator('#dropdown-class-example');
+  await dropdown.selectOption({label:'Option2'});
+  await expect(await dropdown).toHaveValue('option2');
 });
